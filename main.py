01c778c464a842afa56688ea845d356bc257ca38
shohan-safa <shohan921042@outlook.com>
2026-06-10 01:47:19 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot is online!")
+
+app = Application.builder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
